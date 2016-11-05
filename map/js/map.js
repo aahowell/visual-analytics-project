749568aaee0e1c6516735e467de43d7fa8b23cfa
Aleah Howell <aleahhowell93@gmail.com>
2016-11-04 21:05:24 -0400

    Add files via upload

--- map/js/map.js
+++ map/js/map.js
@@ -9,23 +9,10 @@ var map = new Datamap({
         var projection, path;
         projection = d3.geo.equirectangular()
             .center([-32, 79])
-            .scale(15)
+            .scale(element.offsetWidth)
             .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
         path = d3.geo.path()
-                .projection( projection );
+                .projection(projection);
         return {path: path, projection: projection};
         }
     });
-
-
-var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36315921-1']);
-  _gaq.push(['_setDomainName', 'bl.ocks.org']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
